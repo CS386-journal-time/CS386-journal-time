fa074d29ec615eb854755075f98833a286926e83
Shelby <seh428@nau.edu>
2020-07-28 17:23:33 -0500

    changes to map

--- journal_time/lib/JournalEntry.dart
+++ journal_time/lib/JournalEntry.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hsvcolor_picker/flutter_hsvcolor_picker.dart';
 import 'Day.dart';
 import 'package:intl/intl.dart';
-import 'dart:io';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:image_picker/image_picker.dart';
-
-import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
+import 'Photo.dart';
 
 
 class JournalEntry extends StatelessWidget {
   DateTime choiceDay;
   String journalText;
-  File image;
-  final myController = TextEditingController();
-  var photo = Photo();
 
   JournalEntry({this.choiceDay, this.journalText});
 
@@ -26,26 +17,51 @@ class JournalEntry extends StatelessWidget {
     return '$formatted';
   }
 
+  final myController = TextEditingController();
+
   void dispose() {
     myController.dispose();
   }
 
-    Widget textBox() {
-      final int maxLine = 30;
-      return Container(
-        padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
-        height: maxLine * 8.0,
-        child: TextField(
-          controller: myController,
-          keyboardType: TextInputType.multiline,
-          maxLines: maxLine,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Journal Time',
-          ),
+
+  // text input
+  Widget textBox() {
+    final int maxLine = 30;
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
+      height: maxLine * 8.0,
+      child: TextField(
+        controller: myController,
+        keyboardType: TextInputType.multiline,
+        maxLines: maxLine,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Journal Time',
         ),
-      );
-    }
+      ),
+    );
+  }
+
+
+  // scrollable image container
+  Widget imageList() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 6.0),
+      height: 160,
+      child: ListView(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Photo(),
+          Photo(),
+          Photo(),
+        ],
+      ),
+    );
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -54,18 +70,22 @@ class JournalEntry extends StatelessWidget {
       appBar: AppBar(
         title: Text(formatDate()),
       ),
-      body: Column(
+      body: ListView(
+        scrollDirection: Axis.vertical,
         children: <Widget>[
-          textBox(),
-          photo.createElement().build(),
-          photo.createElement().build(),
+          Column(
+            children: <Widget>[
+              textBox(),
+              imageList(),
+            ],
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: (){
+        onPressed: () {
           Route route = MaterialPageRoute(
-            builder: (context) => Day(
-              choiceDay: choiceDay, journalText: myController.text, ),
+            builder: (context) =>
+                mapFeature(),
           );
           Navigator.push(context, route);
         },
@@ -76,98 +96,3 @@ class JournalEntry extends StatelessWidget {
     );
   }
 }
-
-
-class Photo extends StatefulWidget {
-  @override
-  PhotoState createState() => PhotoState();
-}
-
-class PhotoState extends State<Photo> {
-  File _image;
-  final picker = ImagePicker();
-
-  Future _getImageGallery() async {
-    var pickedFile = await picker.getImage(source: ImageSource.gallery);
-
-    setState(() {
-      _image = File(pickedFile.path);
-      print('File: $_image');
-    });
-  }
-
-  File getImage()
-  {
-    return _image;
-  }
-  
-  Widget imageGrid() {
-    return GridView.count(
-      shrinkWrap: true,
-      primary: false,
-      padding: const EdgeInsets.all(10),
-      crossAxisSpacing: 10,
-      crossAxisCount: 3,
-      children: <Widget>[
-        GestureDetector(
-          onTap: _getImageGallery,
-          child: Container(
-            color: Colors.grey[400],
-            child: _image == null ? Icon(FontAwesomeIcons.camera) : Image.file(_image),
-          ),
-        ),
-        GestureDetector(
-          onTap: _getImageGallery,
-          child: Container(
-            color: Colors.grey[400],
-            child: _image == null ? Icon(FontAwesomeIcons.camera) : Image.file(_image),
-          ),
-        ),
-        GestureDetector(
-          onTap: _getImageGallery,
-          child: Container(
-            color: Colors.grey[400],
-            child: _image == null ? Icon(FontAwesomeIcons.camera) : Image.file(_image),
-          ),
-        ),
-      ],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return imageGrid();
-  }
-}
-//Access Map API
-
-Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(title: new Text('Leaflet Maps')),
-        body: new FlutterMap(
-            options: new MapOptions(
-                center: new LatLng(40.71, -74.00), minZoom: 10.0),
-            layers: [
-              new TileLayerOptions(
-                  urlTemplate:
-                  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  subdomains: ['a', 'b', 'c']),
-              new MarkerLayerOptions(markers: [
-                new Marker(
-                    width: 45.0,
-                    height: 45.0,
-                    point: new LatLng(40.73, -74.00),
-                    builder: (context) => new Container(
-                      child: IconButton(
-                        icon: Icon(Icons.location_on),
-                        color: Colors.red,
-                        iconSize: 45.0,
-                        onPressed: () {
-                          print('Marker tapped');
-                        },
-                      ),
-                    ))
-              ])
-            ]));
-
-  }
--- journal_time/lib/mapFeature.dart
+++ journal_time/lib/mapFeature.dart
@@ -7,9 +7,22 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:screenshot/screenshot.dart';
 
-class _mapping {
+class mapFeature extends StatefulWidget {
+
   @override
-  Widget build(BuildContext context) {
+  _mapState createState() => _mapState();
+}
+
+class _mapState extends State<mapFeature> {
+  static File _map;
+  final picker = _LocationPicker();
+
+  setState(() {
+  _map = File(picker.path);
+  });
+
+
+  Future _LocationPicker() async {
     return new Scaffold(
         appBar: new AppBar(title: new Text('Leaflet Maps')),
         body: new FlutterMap(
@@ -40,6 +53,34 @@ class _mapping {
   }
 }
 
+Widget mapCard(File localImage) {
+  return Container(
+    width: 240.0,
+    child: GestureDetector(
+      //onTap: showAlertDialog,
+      child: Container(
+        child: Card(
+          semanticContainer: true,
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          color: Colors.grey[400],
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0)
+          ),
+          child: Container(
+            child: localImage == null
+                ? Icon(FontAwesomeIcons.image)
+                : Image.file(localImage, fit: BoxFit.cover,),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+@override
+Widget build(BuildContext context) {
+  return mapCard(_map);
+}
 
 
 
